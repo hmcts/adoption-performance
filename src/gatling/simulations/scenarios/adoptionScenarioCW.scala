471c7fd00af0d95bc2e33b97d57e69278bf8cdf4
Jack <jack.saville@hmcts.net>
2023-10-10 15:35:30 +0100

    scenerio 2 test

--- src/gatling/simulations/scenarios/adoptionScenarioCW.scala
+++ src/gatling/simulations/scenarios/adoptionScenarioCW.scala
@@ -285,5 +285,232 @@ object adoptionScenarioCW {
       }
       .pause(ThinkTime)
 
+      /*======================================================================================
+   * 'Birth father details' Event
+   ======================================================================================*/
 
+      .group("AD_600_BirthFatherDetails") {
+        exec(http("Birth Father Details")
+          .get(BaseURL + "/la-portal/birth-father/name-on-certificate")
+          .headers(Headers.commonHeader)
+          .check(CsrfCheck.save)
+          .check(substring("Is the birth father's name on the birth certificate?")))
+      }
+      .pause(ThinkTime)
+
+
+
+      /*======================================================================================
+* Is the birth father's name on the birth certificate? - Yes
+======================================================================================*/
+
+      .group("AD_610_IsFatherOnBirthCertificate") {
+        exec(http("Is Father On Birth Certificate")
+          .post(BaseURL + "/la-portal/birth-father/name-on-certificate")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherNameOnCertificate", "Yes")
+          .check(CsrfCheck.save)
+          .check(substring("What is the full name of the child's birth father?")))
+      }
+      .pause(ThinkTime)
+
+
+
+      /*======================================================================================
+* What is the full name of the child's birth father?
+======================================================================================*/
+
+      .group("AD_620_BirthFatherName") {
+        exec(http("Birth Father Name")
+          .post(BaseURL + "/la-portal/birth-father/full-name")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherFirstNames", "birthFatherFirstNames#{randomString}")
+          .formParam("birthFatherLastNames", "birthFatherLastNames#{randomString}")
+          .check(CsrfCheck.save)
+          .check(substring("Is the child's birth father still alive?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* Is the child's birth father still alive? - Yes
+======================================================================================*/
+
+      .group("AD_630_BirthFatherAlive") {
+        exec(http("Birth Father Alive")
+          .post(BaseURL + "/la-portal/birth-father/still-alive")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherStillAlive", "Yes")
+          .formParam("birthFatherUnsureAliveReason", "")
+          .check(CsrfCheck.save)
+          .check(substring("Does the birth father have parental responsibility?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* Does the birth father have parental responsibility? - Yes
+======================================================================================*/
+
+      .group("AD_640_FatherHaveResponsibility") {
+        exec(http("Father Have Parental Responsibility")
+          .post(BaseURL + "/la-portal/birth-father/parental-responsibility")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherResponsibility", "Yes")
+          .check(CsrfCheck.save)
+          .check(substring("How was parental responsibility granted to the birth father?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* How was parental responsibility granted to the birth father? - Court Order
+======================================================================================*/
+
+      .group("AD_650_HowWasResponsibilityGranted") {
+        exec(http("How Was Responsibility Granted")
+          .post(BaseURL + "/la-portal/birth-father/parental-responsibility/granted")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherResponsibilityReason", "")
+          .formParam("birthFatherResponsibilityReason", "")
+          .formParam("birthFatherResponsibilityReason", "")
+          .formParam("birthFatherResponsibilityReason", "")
+          .formParam("birthFatherResponsibilityReason", "")
+          .formParam("birthFatherResponsibilityReason", "Court order")
+          .formParam("birthFatherOtherResponsibilityReason", "")
+          .check(CsrfCheck.save)
+          .check(substring("What is the nationality of the child's birth father?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* What is the nationality of the child's birth Father? - British
+======================================================================================*/
+
+      .group("AD_660_BirthFatherNationality") {
+        exec(http("Birth Father Nationality")
+          .post(BaseURL + "/la-portal/birth-father/nationality")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherNationality", "")
+          .formParam("birthFatherNationality", "")
+          .formParam("birthFatherNationality", "")
+          .formParam("birthFatherNationality", "")
+          .formParam("birthFatherNationality", "British")
+          .formParam("addAnotherNationality", "")
+          .check(CsrfCheck.save)
+          .check(substring("What is the occupation of the child's birth father?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* What is the occupation of the child's birth father?
+======================================================================================*/
+
+      .group("AD_680_BirthFatherOccupation") {
+        exec(http("Birth Father Occupation")
+          .post(BaseURL + "/la-portal/birth-father/occupation")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherOccupation", "birthFatherOccupation#{randomString}")
+          .check(CsrfCheck.save)
+          .check(substring("last known address")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+* Do you have the birth father's last known address?
+======================================================================================*/
+
+      .group("AD_690_BirthFatherAddressKnown") {
+        exec(http("Birth Father Address Known")
+          .post(BaseURL + "/la-portal/birth-father/address-known")
+          .headers(Headers.commonHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherAddressKnown", "Yes")
+          .formParam("birthFatherAddressNotKnownReason", "")
+          .check(CsrfCheck.save)
+          .check(substring("What is the birth father's last known address?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+    * Postcode lookup for Mother
+    ======================================================================================*/
+
+      .group("AD_700_Father_Address_Lookup") {
+        exec(Common.postcodeLookupLa("birth-father", "birthFather"))
+      }
+      .pause(ThinkTime)
+
+      /*======================================================================================
+      * Select Father Address
+      ======================================================================================*/
+
+      .group("AD_710_MotherAddressSelect") {
+        exec(http("Adoption Father's Select")
+          .post(BaseURL + "/la-portal/birth-father/address/select")
+          .headers(Headers.commonHeader)
+          .headers(Headers.postHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherSelectAddress", "#{addressIndex}")
+          .check(substring("When was the last date this address was confirmed?")))
+      }
+      .pause(ThinkTime)
+
+
+      /*======================================================================================
+    * When was the last date this address was confirmed?
+    ======================================================================================*/
+
+      .group("AD_720_AddressWasConfirmed") {
+        exec(http("Address Was Confirmed")
+          .post(BaseURL + "/la-portal/birth-father/last-address-confirmed")
+          .headers(Headers.commonHeader)
+          .headers(Headers.postHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherLastAddressDate-day", "#{randomDay}")
+          .formParam("birthFatherLastAddressDate-month", "#{randomMonth}")
+          .formParam("birthFatherLastAddressDate-year", "#{adoptionYear}")
+          .check(substring("Should the birth father be sent documents or court orders relating to this adoption?")))
+      }
+      .pause(ThinkTime)
+
+
+
+      /*======================================================================================
+  * Should the birth father be sent documents or court orders relating to this adoption? - yes
+  ======================================================================================*/
+
+      .group("AD_730_FatherSentDocuments") {
+        exec(http("Father Be Sent Documents")
+          .post(BaseURL + "/la-portal/birth-father/served-with")
+          .headers(Headers.commonHeader)
+          .headers(Headers.postHeader)
+          .formParam("_csrf", "#{csrfToken}")
+          .formParam("locale", "en")
+          .formParam("birthFatherServedWith", "Yes")
+          .formParam("birthMotherNotServedWithReason", "")
+          .check(regex("""id="birth-father-details-status" class="govuk-tag app-task-list__tag ">Completed""")))
+      }
+      .pause(ThinkTime)
 }
